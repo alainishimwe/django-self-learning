890a47917eccde1f2c9cc46232bc002e5eacf883
Alain Ishimwe <alainishimwe2000@gmail.com>
2023-06-22 09:47:57 -0400

    added abstarct user

--- blog/settings.py
+++ blog/settings.py
@@ -149,3 +149,6 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+AUTH_USER_MODEL = 'posts.User'
