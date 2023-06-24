44a4f6dff970e64ab61b7cf6bad94f98bcec3d6a
Alain Ishimwe <alainishimwe2000@gmail.com>
2023-06-24 12:46:36 -0400

    added generic views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter.order_by('created_on')
+    template_name = 'index.html'
